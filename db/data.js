5749e263de4c4fb18678ad00adc52c13672cde8c
Thiago <tgrimberg@udesa.edu.ar>
2022-09-12 09:59:04 -0300

    detalles del data.js

--- db/data.js
+++ db/data.js
@@ -4,84 +4,84 @@ const data = {
             userId: 1,
             postId: 1,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 3,
             postId: 2,
             likes: 15,
-            comments: ['imasmf', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['imasmf', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 1,
             postId: 3,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 5,
             postId: 4,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 4,
             postId: 5,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 8,
             postId: 6,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 10,
             postId: 7,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 1,
             postId: 8,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 13,
             postId: 9,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 4,
             postId: 10,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 5,
             postId: 11,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
         {
             userId: 2,
             postId: 12,
             likes: 12,
-            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd', 'lorem ipsum']
 
         },
     ],
