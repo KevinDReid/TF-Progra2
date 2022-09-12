19137471ce263ca425a69010ff43f70e0cc29928
KevinDReid <kreid@udesa.edu.ar>
2022-09-12 09:51:13 -0300

    data

---
+++ db/data.js
@@ -0,0 +1,101 @@
+const data = {
+    posts: [
+        {
+            userId: 1,
+            postId: 1,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 3,
+            postId: 2,
+            likes: 15,
+            comments: ['imasmf', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 1,
+            postId: 3,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 5,
+            postId: 4,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 4,
+            postId: 5,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 8,
+            postId: 6,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 10,
+            postId: 7,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 1,
+            postId: 8,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 13,
+            postId: 9,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 4,
+            postId: 10,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 5,
+            postId: 11,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+        {
+            userId: 2,
+            postId: 12,
+            likes: 12,
+            comments: ['siajdoasjd', 'oiasndinas', 'oiasdnoiasnd']
+
+        },
+    ],
+    users: [
+        {
+            username: '',
+            name: '',
+            userId: 1,
+            password: 12345678,
+            email: 'asd@asd.com',
+            birthday: '01-01-1000',
+            dni: 12345678,
+            foto: '',
+            postsNumber: 10
+        },
+    ]
+}
