936c572bd0c3403faf2769d1df9fe71aa07edfad
KevinDReid <kreid@udesa.edu.ar>
2022-09-19 12:00:21 -0300

    emprolijamiento

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const indexController = require("../controllers/indexController");
 
-/* GET home page. */
+
 router.get('/', indexController.reg);
 
 module.exports = router;
