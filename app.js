644014d34fc56bd2c11bb47821f2365e3c6ce329
Rana Moché <fmoche@udesa.edu.ar>
2022-11-20 17:46:24 -0300

    w

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const session = require('express-session');
-
+const db = require('./database/models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
